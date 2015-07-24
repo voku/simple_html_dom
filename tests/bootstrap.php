ea32e72cd2e37c4c40b3f5fc125b095b24cf4ff2
Lars Moelleken <lmoelleken@menadwork.com>
2015-07-24 14:07:37 +0200

    [+]: use "psr-4" autoload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/src/voku/helper/HtmlDomParser.php';
+require_once dirname(__DIR__) . '/src/voku/helper/SimpleHtmlDom.php';
+require_once dirname(__DIR__) . '/src/voku/helper/SimpleHtmlDomNodeBlank.php';
+require_once dirname(__DIR__) . '/src/voku/helper/SimpleHtmlDomNode.php';
