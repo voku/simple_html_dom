c0fc023d99fd8a719937d3aa03d2b596fa972251
Lars Moelleken <lars@moelleken.org>
2016-11-30 18:50:58 +0100

    [+]: add one more test

--- tests/TwigTest.php
+++ tests/TwigTest.php
@@ -21,32 +21,20 @@ class TwigTest extends PHPUnit_Framework_TestCase
     // ------------------
 
     $navigation = $html->find('.navigation--element');
+    $secondAnchor = $navigation[1]->find('a');
 
     self::assertSame('navigation--element', $navigation[0]->class);
     self::assertSame('<a href="{{ item.href }}">{{ item.caption }}</a>', $navigation[0]->innertext);
+    self::assertSame('https://foo?lall=###FOO###&lall={#lall#}#foo', $secondAnchor[0]->href);
 
     // ------------------
     // edit
     // ------------------
 
     $navigation[0]->class = 'fooo';
+    $navigation[1]->class = 'fooo';
 
-    $expected = '<!DOCTYPE html>
-<html>
-<head>
-  <title>My Webpage</title>
-</head>
-<body>
-<ul class="navigation">
-  {% for item in navigation %}
-    <li class="fooo"><a href="{{ item.href }}">{{ item.caption }}</a></li>
-  {% endfor %}
-</ul>
-
-<h1>My Webpage</h1>
-{{ a_variable }}
-</body>
-</html>';
+    $expected = '<!DOCTYPE html> <html> <head><title>My Webpage</title></head> <body> <ul class="navigation">  {% for item in navigation %}     <li class="fooo"><a href="{{ item.href }}">{{ item.caption }}</a></li>     <li class="fooo"><a href="https://foo?lall=###FOO###&lall={#lall#}#foo">link</a></li>  {% endfor %} </ul> <h1>My Webpage</h1> {{ a_variable }} </body> </html>';
 
     self::assertSame(
         str_replace(array("\r\n", "\r", "\n"), ' ', $expected),
