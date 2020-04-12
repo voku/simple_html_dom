17e133395b53978fae11baff35df0857c2e8d94e
Lars Moelleken <lars.moelleken@vdmg-it.com>
2020-04-12 23:49:58 +0200

    [+]: add one more example v2

--- example/example_find_image_if_exists.php
+++ example/example_find_image_if_exists.php
@@ -1,9 +1,5 @@
 <?php
 
-use voku\helper\SimpleHtmlDomInterface;
-use voku\helper\SimpleHtmlDomNode;
-use voku\helper\SimpleHtmlDomNodeInterface;
-
 require_once '../vendor/autoload.php';
 
 // -----------------------------------------------------------------------------
