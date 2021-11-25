6bf336c166953acdf01161ebaeffe400e55a0474
lmoelleken <lmoelleken@meerx-it.com>
2021-11-25 06:01:00 +0100

    [+]: phpstan / phpdoc fixes

--- src/voku/helper/XmlDomParser.php
+++ src/voku/helper/XmlDomParser.php
@@ -89,7 +89,7 @@ class XmlDomParser extends AbstractDomParser
      * @throws \BadMethodCallException
      * @throws \RuntimeException
      *
-     * @return XmlDomParser
+     * @return static
      */
     public static function __callStatic($name, $arguments)
     {
@@ -499,7 +499,7 @@ class XmlDomParser extends AbstractDomParser
      * @param string   $html
      * @param int|null $libXMLExtraOptions
      *
-     * @return self
+     * @return $this
      */
     public function loadHtml(string $html, $libXMLExtraOptions = null): DomParserInterface
     {
@@ -516,7 +516,7 @@ class XmlDomParser extends AbstractDomParser
      *
      * @throws \RuntimeException
      *
-     * @return XmlDomParser
+     * @return $this
      */
     public function loadHtmlFile(string $filePath, $libXMLExtraOptions = null): DomParserInterface
     {
@@ -576,7 +576,7 @@ class XmlDomParser extends AbstractDomParser
      * @param string   $xml
      * @param int|null $libXMLExtraOptions
      *
-     * @return XmlDomParser
+     * @return $this
      */
     public function loadXml(string $xml, $libXMLExtraOptions = null): self
     {
@@ -593,7 +593,7 @@ class XmlDomParser extends AbstractDomParser
      *
      * @throws \RuntimeException
      *
-     * @return XmlDomParser
+     * @return $this
      */
     public function loadXmlFile(string $filePath, $libXMLExtraOptions = null): self
     {
