143d60b5a8a8e906314825fa876e19c869210698
Lars Moelleken <lars@moelleken.org>
2024-07-03 17:59:15 +0200

    [+]: added a test for the last error from `lHtmlDomHelper::mergeHtmlAttributes()`

--- tests/SimpleHtmlHelperTest.php
+++ tests/SimpleHtmlHelperTest.php
@@ -19,4 +19,15 @@ final class SimpleHtmlHelperTest extends TestCase
 
         self::assertSame('<div class="foo foo2" id="bar" data-foo="bar" data-lall="foo"></div>', $result);
     }
+
+    public function testMergeHtmlAttributesWithZeroValues()
+    {
+        $result = HtmlDomHelper::mergeHtmlAttributes(
+            '<input type="checkbox" class="foo" id="bar" data-foo="bar"></input>',
+            'class="foo2" data-lall=0 value="0"',
+            '#bar'
+        );
+
+        self::assertSame('<input type="checkbox" class="foo foo2" id="bar" data-foo="bar" data-lall="0" value="0">', $result);
+    }
 }
